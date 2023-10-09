96bba4c1cc75af2ec3a17572684c9d95000a1118
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-10-09 22:25:47 +0100

    chunk

--- exercises/6-chunk/index.js
+++ exercises/6-chunk/index.js
@@ -9,17 +9,29 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-  const chunksParent = [];
+  const chunked = [];
 
-  for (let index in array) {
-    const chunk = array.slice(index * size, size + size * index);
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
 
-    if (chunk.length) {
-      chunksParent.push(chunk);
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
     }
   }
 
-  return chunksParent;
+  return chunked;
 }
 
+chunk([1, 2, 3, 4, 5], 4);
+
 module.exports = chunk;
+
+//   for (let index in array) {
+//     const chunk = array.slice(index * size, size + size * index);
+
+//     if (chunk.length) {
+//       chunksParent.push(chunk);
+//     }
+//   }
