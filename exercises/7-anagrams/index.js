b13dae53fd2b450d80a71ad5f040db5408016ca7
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-10-10 23:31:02 +0100

    7-anagrams

--- exercises/7-anagrams/index.js
+++ exercises/7-anagrams/index.js
@@ -8,6 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const createStringObject = (string) => {
+  const object = {};
+  for (let char of string) {
+    if (!object.hasOwnProperty(char)) {
+      object[char] = 0;
+    }
+    object[char]++;
+  }
+
+  return object;
+};
+
+function anagrams(stringA, stringB) {
+  const regex = /[^\w]/g;
+
+  const NewStringA = stringA.replace(regex, "").toLowerCase();
+  const NewStringB = stringB.replace(regex, "").toLowerCase();
+
+  if (NewStringA.length !== NewStringA.length) {
+    return false;
+  } else {
+    let ObjectA = createStringObject(NewStringA);
+    let ObjectB = createStringObject(NewStringB);
+
+    let isAnagram = true;
+
+    for (let char in ObjectA) {
+      for (let char2 in ObjectB) {
+        if (ObjectA[char] !== ObjectB[char]) {
+          isAnagram = false;
+        }
+      }
+    }
+
+    return isAnagram;
+  }
+}
+
+// anagrams("RAIL! SAFETY!", "fairy tales");
+// anagrams("rail safety", "fairy tales");
+anagrams("Hi there", "Bye there");
 
 module.exports = anagrams;
