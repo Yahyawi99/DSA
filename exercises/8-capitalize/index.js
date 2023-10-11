4e0d17b10d6006ee118a4234b8889d82fb72c3c5
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-10-11 20:17:37 +0100

    capitalize

--- exercises/8-capitalize/index.js
+++ exercises/8-capitalize/index.js
@@ -7,6 +7,20 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const strArr = str.split(" ").map((word) => {
+    return word.replace(word[0], word[0].toUpperCase());
+  });
+
+  //   const newStr = [];
+  //   for (let char of strArr) {
+  //     char = char.replace(char[0], char[0].toUpperCase());
+  //     console.log(strArr);
+  //   }
+
+  console.log(strArr);
+}
+
+capitalize("i love breakfast at bill miller bbq");
 
 module.exports = capitalize;
