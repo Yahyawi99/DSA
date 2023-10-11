3720ba2c444974de312a91c96282a9703d074dbe
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-10-11 21:05:53 +0100

    steps

--- exercises/9-steps/index.js
+++ exercises/9-steps/index.js
@@ -17,7 +17,20 @@
 //       '### '
 //       '####'
 
-const hashTags = (hash, space) => {
+function steps(n, step = 1) {
+  if (step === n) {
+    return;
+  }
+
+  steps(n - 1);
+}
+
+steps(10);
+
+module.exports = steps;
+
+/*
+const step = (hash, space) => {
   let result = "";
 
   while (hash > 0) {
@@ -35,8 +48,23 @@ const hashTags = (hash, space) => {
 
 function steps(n) {
   for (let i = 1, j = n - 1; i <= n; i++, j--) {
-    console.log(hashTags(i, j));
+    console.log(step(i, j));
   }
 }
+*/
 
-module.exports = steps;
+/*
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
+}
+*/
